f773bfcff9dfa87fb301cb3036b6ba0dad7aaed7
Dilip Raghavan <dilip.raghavan.cse@gmail.com>
2021-02-25 11:45:24 -0500

    Set up for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 
 //Set iew engine as HBS.
@@ -91,7 +92,7 @@ app.get('*',(req,res) =>{
 });
 
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log('App started at port 3000');
 }
 );
